2633d637c954095c469cbcb4fe30355db1b40e64
Ronny Fuchs <ro.fuchs585@googlemail.com>
2026-09-09 22:37:41 +0200

    fix prek

--- custom_components/remko_http/button.py
+++ custom_components/remko_http/button.py
@@ -7,12 +7,12 @@ import logging
 
 from homeassistant.components.button import ButtonEntity
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import (
-    DOMAIN,
     BUTTONS,
+    DOMAIN,
     RemkoButtonDef,
 )
 from .coordinator import RemkoCoordinator
